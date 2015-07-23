5c8f13ffbeb6faa4d40570d3e870acf854d55c35
Kevin Phair <kevin.phair@gmail.com>
2015-07-23 10:45:14 +0100

    Added scoreboard methods

--- Code Challenge - Robot Wars/src/com/kevinphair/robotwars/ScoreBoard.java
+++ Code Challenge - Robot Wars/src/com/kevinphair/robotwars/ScoreBoard.java
@@ -12,7 +12,7 @@ public class ScoreBoard {
 	static int[] draws = new int[numTeams];
 	static int[] losses = new int[numTeams];
 	static String[] teamName = new String[numTeams];
-	
+
 	public ScoreBoard (int teams) {
 		this.numTeams = teams;
 		for (int i = 0; i < numTeams; ++i) {
@@ -22,9 +22,51 @@ public class ScoreBoard {
 			teamName[i] = "";
 		}
 	}
-	
+
 	public void setName (int team, String name) {
 		this.teamName[team] = name;
 	}
-	
+
+
+	public void addWin (int teamNumber){
+
+		wins[teamNumber]++;
+
+	}
+
+
+	public void addLoss(int teamNumber){
+
+		losses[teamNumber]++;
+
+	}
+
+
+	public void addDraw (int teamNumber){
+
+		draws[teamNumber]++;
+
+	}
+
+
+	public int getWins(int teamNumber) {
+
+		return wins[teamNumber];
+
+	}
+
+
+	public int getLosses(int teamNumber) {
+
+		return losses[teamNumber];
+
+	}
+
+	public int getDraws(int teamNumber) {
+
+		return draws[teamNumber];
+
+	}
+
+
 }
