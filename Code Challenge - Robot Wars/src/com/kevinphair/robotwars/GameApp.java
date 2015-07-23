af73023075f249304fb295672b9821823c5d9b88
Kevin Phair <kevin.phair@gmail.com>
2015-07-23 10:46:15 +0100

    Added combat methods and successfully tested

--- Code Challenge - Robot Wars/src/com/kevinphair/robotwars/GameApp.java
+++ Code Challenge - Robot Wars/src/com/kevinphair/robotwars/GameApp.java
@@ -1,5 +1,7 @@
 package com.kevinphair.robotwars;
 
+import java.util.Random;
+
 /**
  * @author Kevin Phair, Sam Downes
  * Created 23 Jul 2015
@@ -47,35 +49,104 @@ public class GameApp {
 	
 	public static void main(String[] args) {
 
+		new GameApp().runProgram();
+		
+	
 	}
 
 	public void runProgram() {
 		
+		sb = new ScoreBoard(2);
+		
 		Contestant[] robotArmy = initialiseArmy();
 		Contestant[] humanArmy = initialiseArmy();
+		int result = doBattle(robotArmy, humanArmy);
+		System.out.println(result);
+		if(result < 0){
+			System.out.println("The robot army looses");
+			
+		}else if(result > 0){
+			System.out.println("The robot army wins");
+			
+		}else{
+			System.out.println("The armies have drawn");
+		}
+			
 	}
+	
+	/**
+	 * 
+	 * @return
+	 */
 	
 	public Contestant[] initialiseArmy() {
 		
+		Contestant [] army = new Contestant[500];
+		for(int i = 0; i < army.length; ++i){
+			army[i] = new Contestant(new Random().nextInt(101),"" + i);
+		}
+		return army;
+		
 	}
+	
+	/**
+	 * 
+	 * @param vs1
+	 * @param vs2
+	 * @return
+	 */
 	
 	public int doBattle(Contestant[] vs1, Contestant[] vs2) {
 		for (int i = 0; i < vs1.length; ++i) {
 			int result = fight(vs1[i], vs2[i]);
 			// vs1 loses (vs2 wins)
 			if (result < 0) {
+				sb.addWin(1);
+				sb.addLoss(0);
 				
 			// vs1 wins (vs2 loses)
 			} else if (result > 0) {
+				sb.addWin(0);
+				sb.addLoss(1);
 				
 			// vs1 draws (vs2 draws)
 			} else {
+				sb.addDraw(0);
+				sb.addDraw(1);
 				
 			}
+			System.out.println(sb.getWins(0) + " - " + sb.getWins(1));
+			
 		}
+		if(sb.getWins(0) > sb.getWins(1)){
+			return 1;
+		}else if (sb.getWins(1) > sb.getWins(0)){
+			return - 1;
+		}else {
+			return 0;
+		}
+	
 	}
 	
 	public int fight(Contestant vs1, Contestant vs2) {
+		
+		int vs1Power = vs1.getPower();
+		int vs2Power = vs2.getPower();
+		
+		
+		if(vs1.getPower() > vs2.getPower()){
+			System.out.println(vs1Power + " > " + vs2Power);
+			return 1;			
+		}else if (vs2.getPower() > vs1.getPower()){
+			System.out.println(vs1Power + " < " + vs2Power);
+			return -1;
+		}else{
+			System.out.println(vs1Power + " = " + vs2Power);
+			return 0;
+		}
+	
+		
+		
 		
 	}
 	
