64d15ca175403475ddd7648c9d468d52bb1accc2
Kevin Phair <kevin.phair@gmail.com>
2015-07-23 12:32:35 +0100

    Added methods for last army standing

--- Code Challenge - Robot Wars/src/com/kevinphair/robotwars/GameApp.java
+++ Code Challenge - Robot Wars/src/com/kevinphair/robotwars/GameApp.java
@@ -1,6 +1,7 @@
 package com.kevinphair.robotwars;
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * @author Kevin Phair, Sam Downes
@@ -49,39 +50,134 @@ public class GameApp {
 	
 	public static void main(String[] args) {
 
-		new GameApp().runProgram();
 		
-	
+		while (true) {
+			
+			switch(new GameApp().mainMenu()) {
+				case 1:
+					new GameApp().runProgram();
+					break;
+				case 2:
+					new GameApp().lastArmyStanding();
+					break;
+				case 3:
+					System.out.println("Bye...");
+					return;
+			}
+		}
 	}
 
+	public int mainMenu() {
+		
+		Scanner scan = new Scanner(System.in);
+		int choice = 0;
+		
+		System.out.println("Enter the number of the game type you wish to see and press ENTER");
+		System.out.println("");
+		System.out.println("1: Single battle between Robot and Human armies");
+		System.out.println("2: Last army standing");
+		System.out.println("3: Quit");
+		System.out.println("");
+		
+		while (true) {
+			if (scan.hasNextInt()) {
+				choice = scan.nextInt();
+			}
+			if (choice >= 1 && choice <= 3) {
+				return choice;
+			} else {
+				System.out.println("Invalid choice. Please try again");
+			}
+		}
+	}
+	
 	public void runProgram() {
 		
 		sb = new ScoreBoard(2);
 		
 		Contestant[] robotArmy = initialiseArmy();
+		for (Contestant c : robotArmy) {
+			c.setName(makeRobotName());
+		}
 		Contestant[] humanArmy = initialiseArmy();
+		for (Contestant c : humanArmy) {
+			c.setName(makeHumanName());
+		}
 		int result = doBattle(robotArmy, humanArmy);
-		System.out.println(result);
+
 		if(result < 0){
-			System.out.println("The robot army looses");
+			System.out.print("The human army won this battle with ");
+			System.out.println(sb.getWins(1) + " wins, " + sb.getLosses(1) + " losses and " + sb.getDraws(1) + " draws (" + getTotalPower(humanArmy) + " total power)");
+			System.out.println("Robots lost with " + sb.getWins(0) + " wins, " + sb.getLosses(0) + " losses and " + sb.getDraws(0) + " draws (" + getTotalPower(robotArmy) + " total power)");
 			
 		}else if(result > 0){
-			System.out.println("The robot army wins");
+			System.out.print("The robot army won this battle with ");
+			System.out.println(sb.getWins(0) + " wins, " + sb.getLosses(0) + " losses and " + sb.getDraws(0) + " draws (" + getTotalPower(robotArmy) + " total power)");
+			System.out.println("Humans lost with " + sb.getWins(1) + " wins, " + sb.getLosses(1) + " losses and " + sb.getDraws(1) + " draws (" + getTotalPower(humanArmy) + " total power)");
 			
 		}else{
-			System.out.println("The armies have drawn");
+			System.out.println("Both armies have drawn this battle");
 		}
 			
 	}
 	
+	public void lastArmyStanding() {
+
+		ScoreBoard overallSb = new ScoreBoard(2);
+		
+		Contestant[] robotArmy = initialiseArmy();
+		for (Contestant c : robotArmy) {
+			c.setName(makeRobotName());
+		}
+		Contestant[] humanArmy = initialiseArmy();
+		for (Contestant c : humanArmy) {
+			c.setName(makeHumanName());
+		}
+		
+		while (true) {
+
+			sb = new ScoreBoard(2);
+
+			int result = doBattle(robotArmy, humanArmy);
+			// vs1 loses (vs2 wins)
+			if (result < 0) {
+				overallSb.addWin(1);
+				overallSb.addLoss(0);
+			// vs1 wins (vs2 loses)
+			} else if (result > 0) {
+				overallSb.addWin(0);
+				overallSb.addLoss(1);
+			// vs1 draws (vs2 draws)
+			} else {
+				overallSb.addDraw(0);
+				overallSb.addDraw(1);
+			}
+			System.out.println(overallSb.getWins(0) + " - " + overallSb.getWins(1));
+			if (getRemainingPower(robotArmy) == 0 || getRemainingPower(humanArmy) == 0) break;
+		}
+		
+		if(overallSb.getWins(0) < overallSb.getDraws(1)){
+			System.out.print("The human army won the war with ");
+			System.out.println(overallSb.getWins(1) + " battles won, " + overallSb.getLosses(1) + " battles lost and " + overallSb.getDraws(1) + " battles drawn (" + getTotalPower(humanArmy) + " total power)");
+			System.out.println("Robots lost with " + overallSb.getWins(0) + " battles won, " + overallSb.getLosses(0) + " battles lost and " + overallSb.getDraws(0) + " battles drawn (" + getTotalPower(robotArmy) + " total power)");
+			
+		}else if(overallSb.getWins(0) > overallSb.getDraws(1)){
+			System.out.print("The robot army won the war with ");
+			System.out.println(overallSb.getWins(0) + " battles won, " + overallSb.getLosses(0) + " battles lost and " + overallSb.getDraws(0) + " battles drawn (" + getTotalPower(robotArmy) + " total power)");
+			System.out.println("Humans lost with " + overallSb.getWins(1) + " battles won, " + overallSb.getLosses(1) + " battles lost and " + overallSb.getDraws(1) + " battles drawn (" + getTotalPower(humanArmy) + " total power)");
+			
+		}else{
+			System.out.println("Both armies have declared a truce in this war");
+		}
+		
+	}
+	
 	/**
-	 * 
 	 * @return
 	 */
-	
 	public Contestant[] initialiseArmy() {
 		
-		Contestant [] army = new Contestant[500];
+		Contestant[] army = new Contestant[500];
 		for(int i = 0; i < army.length; ++i){
 			army[i] = new Contestant(new Random().nextInt(101),"" + i);
 		}
@@ -90,12 +186,10 @@ public class GameApp {
 	}
 	
 	/**
-	 * 
 	 * @param vs1
 	 * @param vs2
 	 * @return
 	 */
-	
 	public int doBattle(Contestant[] vs1, Contestant[] vs2) {
 		for (int i = 0; i < vs1.length; ++i) {
 			int result = fight(vs1[i], vs2[i]);
@@ -115,8 +209,7 @@ public class GameApp {
 				sb.addDraw(1);
 				
 			}
-			System.out.println(sb.getWins(0) + " - " + sb.getWins(1));
-			
+//			System.out.println(sb.getWins(0) + " - " + sb.getWins(1));
 		}
 		if(sb.getWins(0) > sb.getWins(1)){
 			return 1;
@@ -133,15 +226,22 @@ public class GameApp {
 		int vs1Power = vs1.getPower();
 		int vs2Power = vs2.getPower();
 		
+		String vs1Name = vs1.getName();
+		String vs2Name = vs2.getName();
 		
-		if(vs1.getPower() > vs2.getPower()){
-			System.out.println(vs1Power + " > " + vs2Power);
+		vs1.setPower((vs1Power * 90) / 100);
+		vs2.setPower((vs2Power * 90) / 100);
+		
+		if(vs1Power > vs2Power){
+			System.out.println(vs1Name + " (" + vs1Power + ") > " + vs2Name + " (" + vs2Power + ") ");
+			vs2.makeInactive();
 			return 1;			
-		}else if (vs2.getPower() > vs1.getPower()){
-			System.out.println(vs1Power + " < " + vs2Power);
+		}else if (vs2Power > vs1Power){
+			System.out.println(vs1Name + " (" + vs1Power + ") < " + vs2Name + " (" + vs2Power + ") ");
+			vs1.makeInactive();
 			return -1;
 		}else{
-			System.out.println(vs1Power + " = " + vs2Power);
+			System.out.println(vs1Name + " (" + vs1Power + ") = " + vs2Name + " (" + vs2Power + ") ");
 			return 0;
 		}
 	
@@ -149,5 +249,51 @@ public class GameApp {
 		
 		
 	}
+
+	public String makeRobotName() {
+		String[] sounds = {"ta", "be", "ca", "yo", "shi", "mu", "ra"
+		};
+		
+		int numSounds = 2 + new Random().nextInt(4);
+		String name = "";
+		for (;numSounds > 0; --numSounds) {
+			name += sounds[new Random().nextInt(sounds.length)];
+		}
+		return name+ "-"+new Random().nextInt(20) * 100;
+	}
 	
+	public String makeHumanName() {
+		String vowels = "aeiou";
+		String consonants = "bcdfghjklmnpqrstvwxyz";
+		
+		int numSounds = 1 + new Random().nextInt(4);
+		String name = "";
+		for (;numSounds > 0; --numSounds) {
+			int vpos = new Random().nextInt(vowels.length());
+			int cpos = new Random().nextInt(consonants.length());
+			if (new Random().nextInt(10) > 5) {
+				name += vowels.substring(vpos, vpos+1) + consonants.substring(cpos, cpos+1);
+			} else {
+				name += consonants.substring(cpos, cpos+1) + vowels.substring(vpos, vpos+1);
+			}
+		}
+		return name;
+	}
+	
+	public int getTotalPower(Contestant[] army) {
+		int totalPower = 0;
+		for (Contestant c : army) {
+			totalPower += c.getPower();
+		}
+		return totalPower;
+	}
+
+	public int getRemainingPower(Contestant[] army) {
+		int remainingPower = 0;
+		for (Contestant c : army) {
+			if (c.isActive()) remainingPower += c.getPower();
+		}
+		return remainingPower;
+	}
+
 }
