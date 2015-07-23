fb8deb679497fad8c92d263a9800356ae7b312d4
Mubasher Mohammed <mubasherm2015@gmail.com>
2015-07-23 10:43:28 +0100

    Create Human.java

---
+++ Human.java
@@ -0,0 +1,31 @@
+package com.mubasher.ocja.game;
+
+import java.util.Random;
+
+public class Human {
+	private String name;
+	private int power;
+
+	public Human(String name) {
+		super();
+		this.name = name;
+		
+		Random ran = new Random();
+		power = ran.nextInt(101);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPower() {
+		return power;
+	}
+	public void setPower(int power) {
+		this.power = power;
+	}
+	
+
+}
